1ca77663e1b5273469696cb3aa4537804b25c034
mami <mamamamani43@gmail.com>
2014-11-19 15:12:32 +0900

    first user

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-Codemate.application.configure do
+Codemate::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
