2964a66ef955cc90adde2a4f2c337744dec8f49c
mami <mamamamani43@gmail.com>
2014-11-25 19:44:11 +0900

    contacts消した

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141125071234) do
+ActiveRecord::Schema.define(version: 20141125103644) do
 
   create_table "answers", force: true do |t|
     t.integer  "user_id"
@@ -19,12 +19,6 @@ ActiveRecord::Schema.define(version: 20141125071234) do
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "contacts", force: true do |t|
-    t.string "name",    null: false
-    t.string "email",   null: false
-    t.text   "content", null: false
   end
 
   create_table "favorites", force: true do |t|
