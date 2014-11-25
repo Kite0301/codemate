664aacb73fd46364a50e575feb111ccc5c3cf489
mami <mamamamani43@gmail.com>
2014-11-25 20:58:25 +0900

    mail機能つけました。

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,20 @@ Codemate::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => 'smtp.muumuu-mail.com',
+    :port => 465,
+    :domain => 'prog-8.com',
+    :authentication => 'plain',
+    :user_name => 'info@prog-8.com',
+    :password => 'zeroxone',
+    :ssl => true,
+    :tls => true
+  }
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
