275e01008481417c27f09abc3a537e98c404075f
Kaito <kaito.titech@gmail.com>
2014-12-01 05:01:25 +0900

    tag

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,6 +1,6 @@
 class AboutController < ApplicationController
   def index
-  	@feed_posts = Post.paginate(page: params[:page])
+  	@feed_posts = Post.paginate(page: params[:page] , :per_page => 10)
   	@tags = Post.tag_counts_on(:tags).order('count DESC')
   	
     if signed_in?
