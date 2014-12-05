022ed5f88820ef3f109ce5a5710eb66b0437b100
mami <mamamamani43@gmail.com>
2014-12-05 19:29:04 +0900

    btn

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -4,14 +4,19 @@ $(function(){
         $(this).siblings('.answer').slideToggle();
     });
     $('#show-favorite').hide();
+    $('#show-question-btn').addClass('timeline-btn-new');
     $('#show-favorite-btn').click(function(){
     	$('#show-question').hide();
     	$('#show-favorite').show();
+    	$('#show-favorite-btn').addClass('timeline-btn-new');
+    	$('#show-question-btn').removeClass('timeline-btn-new');
     	return false;
     });
     $('#show-question-btn').click(function(){
     	$('#show-question').show();
     	$('#show-favorite').hide();
+    	$('#show-question-btn').addClass('timeline-btn-new');
+    	$('#show-favorite-btn').removeClass('timeline-btn-new');
     	return false;
     });
 
