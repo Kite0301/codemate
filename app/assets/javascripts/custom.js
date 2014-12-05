6286174bb89e2fc7cc2f3cd34bd2a07b97cc6809
mami <mamamamani43@gmail.com>
2014-12-05 18:25:34 +0900

    tochu

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -3,4 +3,16 @@ $(function(){
         // siblingsは兄弟
         $(this).siblings('.answer').slideToggle();
     });
+    $('#show-favorite').hide();
+    $('#show-favorite-btn').click(function(){
+    	$('#show-question').hide();
+    	$('#show-favorite').show();
+    	return false;
+    });
+    $('#show-question-btn').click(function(){
+    	$('#show-question').show();
+    	$('#show-favorite').hide();
+    	return false;
+    });
+
 });
