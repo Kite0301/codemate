822dfbecc68e8b9a05eda75ebcd051ef4ac67ab0
Kaito <kaito.titech@gmail.com>
2014-12-08 13:11:30 +0900

    改善

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,8 +2,9 @@ class TagsController < ApplicationController
 	before_action :set_tag, only: [:show]
   
   def show
-  	@posts = Post.tagged_with(@tag.name)
+  	@posts = Post.tagged_with(@tag.name).paginate(page: params[:page], :per_page => 10)
   end
+  
   private
   def set_tag
       @tag = ActsAsTaggableOn::Tag.find(params[:id])
