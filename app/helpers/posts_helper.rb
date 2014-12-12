fe127fe43cb7eda33d7755cff4e7e90f93792893
mami <mamamamani43@gmail.com>
2014-12-12 19:32:48 +0900

    image

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,3 +1,7 @@
 module PostsHelper
 	include ActsAsTaggableOn::TagsHelper
+
+	def question_image_post_for(post, size = '52')
+    image_tag "/docs/#{post.image_post}", size: size, class: :gravatar
+  end
 end
