fd395710888509cefdd908893f9753ae05c03d0b
Kaito <kaito.titech@gmail.com>
2014-12-20 17:06:27 +0900

    login

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 Codemate::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+  devise_scope :user do
+    get "signout" => "devise/sessions#destroy"
+    get "signin" => "devise/sessions#new"
+  end
+
   get "tags/search"
   get "tag_search/index"
   resources :inquiries
 
   get "search/index"
   resources :answers
-
-  get "sessions/new"
-  get "sessions/create"
-  get "sessions/destroy"
   
   match '/explanation', to:'sessions#explanation', via: 'get'
   match '/about', to:'about#index', via:'get'
-  match '/signin', to:'sessions#new', via:'get'
-  match '/signout', to:'sessions#destroy', via:'get'
   match '/search', to: 'search#index', via: 'get' 
   match '/posts/sort/:type', to: 'posts#sort', via: 'get', as: 'sort_post'
   match 'posts/best_answer', to: 'posts#best_answer', via: 'get'
@@ -23,7 +23,6 @@ Codemate::Application.routes.draw do
   resources :tags
   resources :posts
 
-  resources :sessions, only: [:new, :create, :destroy]
   resources :favorites, only: [:create, :destroy]
   resources :users do
     member do
